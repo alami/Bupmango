f1b958fdd8c8d485513656a65c14c1dd8d705c63
alami <alami@km.ru>
2022-04-12 15:40:40 +0500

    ch04-004 CRUD-ProductIndex

---
+++ Bubmango.Web/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Bubmango.Web.Services.IServices;
+using Bupmango.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Bubmango.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto> list = new ();
+            var response = await _productService.GetAllProductsAsync<ResponseDto>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+    }
+}
